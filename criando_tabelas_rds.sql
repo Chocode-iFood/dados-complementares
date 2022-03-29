49747b8f86e5b2842a019f672c49e6762cda8529
silvano.araujo <silvanoeng@hotmail.com>
2022-03-29 10:29:31 -0300

    Inserindo sript de criacao de tabelas mysql

---
+++ criando_tabelas_rds.sql
@@ -0,0 +1,59 @@
+/*Grupo 4 - Projeto Integrador
+Nomes: Claudia Nogueira dos Anjos, Daniel Silveira, Raphaela Leite, Silvano Araujo, Vitor Eleuterio.*/
+
+/* Criando o banco de dados */
+CREATE DATABASE IF NOT EXISTS chocode;
+/* Selecionando o banco de dados que vou trabalhar */
+USE chocode;
+
+/* Criando a tabela cliente */
+CREATE TABLE cliente (
+	id BIGINT NOT NULL AUTO_INCREMENT,
+    nome VARCHAR(100) NOT NULL,
+    endereco VARCHAR(255) NOT NULL,
+    longitude VARCHAR(50) NOT NULL,
+    latitude VARCHAR(50) NOT NULL,
+    PRIMARY KEY (id)
+	) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+    
+        /* Criando a tabela entregador */
+CREATE TABLE entregador (
+	id BIGINT NOT NULL AUTO_INCREMENT,
+    nome VARCHAR(150) NOT NULL,
+    email VARCHAR(150) NOT NULL UNIQUE,
+    senha VARCHAR(255) NOT NULL,
+    url_image VARCHAR(255) NOT NULL,
+    PRIMARY KEY (id)
+	) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+    
+    /* Criando a tabela pedido */
+CREATE TABLE pedido (
+	id BIGINT NOT NULL AUTO_INCREMENT,
+    cliente_id BIGINT NOT NULL,
+    entregador_id BIGINT NOT NULL,
+    nome_restaurante VARCHAR(150) NOT NULL,
+    produto VARCHAR(150) NOT NULL,
+    status VARCHAR(150) NOT NULL,
+    PRIMARY KEY (id),
+    KEY id_cliente_idx (cliente_id),
+    KEY id_entregador_idx (entregador_id),
+    FOREIGN KEY (entregador_id) REFERENCES entregador (id),
+	FOREIGN KEY (cliente_id) REFERENCES cliente (id)
+	) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+    
+    
+      /* Criando a tabela geolocalizacao */
+CREATE TABLE geolocalizacao (
+	id BIGINT NOT NULL AUTO_INCREMENT,
+    pedido_id BIGINT NOT NULL,
+    entregador_id BIGINT NOT NULL,
+    longitude VARCHAR(50) NOT NULL,
+    latitude VARCHAR(50) NOT NULL,
+    data DATETIME NOT NULL,
+    PRIMARY KEY (id),
+    KEY id_pedido_idx (pedido_id),
+    KEY id_entregador_idx (entregador_id),
+    FOREIGN KEY (pedido_id) REFERENCES pedido (id),
+    FOREIGN KEY (entregador_id) REFERENCES entregador (id)
+	) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+    
